c4dbf763d51164c3cfa1bcbf2edb5dbe1e419423
eksre1 <57536612+eksre1@users.noreply.github.com>
2020-07-19 01:49:09 +0400

    Add files via upload

---
+++ RaidBot.py
@@ -0,0 +1,14 @@
+﻿import requests
+import time
+apiurl = 'https://api.vk.com/method/'
+token = 'token'
+chatid = 1
+text = 'жопаписька'
+
+while True:
+	one = requests.get(apiurl + 'messages.send', {'v': 5.67, 'access_token': token, 'chat_id': 
+			chatid, 'message': 'a'})
+	two = requests.get(apiurl + 'messages.edit', {'v': 5.67, 'access_token': token, 'peer_id': 
+			2000000000 + chatid, 'message_id': one.json()['response'], 'message': text})
+	print(two.json())
+	time.sleep(7)
